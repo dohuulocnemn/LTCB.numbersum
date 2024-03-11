70d68cfe19df1dfa61392b9ae58070444b0a3322
ADMIN <ADMIN@LAB1-MAY06>
2024-03-11 08:17:12 +0700

    Add project files.

---
+++ LTCB.numbersum/LTCB.numbersum.cpp
@@ -0,0 +1,16 @@
+// LTCB.numbersum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() { 
+	double so,tong = 0;
+	cout << "Nhap so:";
+	while (so; = 0) {
+		cout << "Nhapso:"; cin >> so;
+		tong t = so;
+	}
+	cout << "tong" << tong << end
+		return 0;
+
+
